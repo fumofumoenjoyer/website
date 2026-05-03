026b7acc697ee70ec5abbf277b11aea6cd1df551
FumoFumoEnjoyer <FumoFumoEnjoyer@fumofumo.dev>
2026-05-03 15:12:11 -0300

    yay

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "FumoFumoEnjoyer Lightweight Personal Website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShell {
+          packages = with pkgs.${system}; [
+            darkhttpd
+            imagemagick
+          ];
+
+          shellHook = ''
+            echo "ᗜˬᗜ Welcome to the FumoFumoEnjoyer website dev shell ᗜˬᗜ"
+            echo "You can preview the site by running: darkhttpd . --port 8080"
+          '';
+        };
+      });
+    };
+}
