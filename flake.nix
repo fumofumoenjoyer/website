e86770d1edada4bb2c2f57ead701c5f27f5085d1
FumoFumoEnjoyer <FumoFumoEnjoyer@fumofumo.dev>
2026-05-03 18:09:09 -0300

    final touches

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
-  description = "FumoFumoEnjoyer Lightweight Personal Website";
+  description = "FumoFumoEnjoyer's Personal Website Dev Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
+    in
+    {
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShell {
           packages = with pkgs.${system}; [
